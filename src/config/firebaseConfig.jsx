65d8a4889ea31167a6462d3dbdc0cafde4b7b631
Artem-makarevich <ipz214_mav@student.ztu.edu.ua>
2024-07-30 23:21:20 +0300

    Update firebaseConfig.jsx

--- src/config/firebaseConfig.jsx
+++ src/config/firebaseConfig.jsx
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxA0j-DYlOq2UhS-cSyXj5xcoruW0EaHM",
-    authDomain: "schedule-system-60bf3.firebaseapp.com",
-    projectId: "schedule-system-60bf3",
-    storageBucket: "schedule-system-60bf3.appspot.com",
-    messagingSenderId: "71974223762",
-    appId: "1:71974223762:web:ddb38d27a28f5857c60b4b",
-    measurementId: "G-GGB1QT1VZH"
+   
 };
 
 
